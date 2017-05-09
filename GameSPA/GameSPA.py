e9b6c6f9e5679e475cefc22657afc983128e7fa3
dsandersAzure <dsanders_can@outlook.com>
2017-05-09 14:27:03 -0400

    First commit. Adding structure to SPA

---
+++ GameSPA/GameSPA.py
@@ -0,0 +1,8 @@
+import json
+from flask import render_template
+from flask.views import MethodView
+
+
+class GameSPA(MethodView):
+    def get(self):
+        return render_template("hello.html")
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import render_template
+from initialization_package import app
+from GameSPA.GameSPA import GameSPA
+
+# Add a game view. The game view is actually contained within a class
+# based on a MethodView. See flask_controllers/GameController.py
+game_view = GameSPA.as_view('Game')
+app.add_url_rule(
+    '/',
+    view_func=game_view,
+    methods=["GET", "POST"]
+)
+
+@app.route('/test')
+def test():
+    print()
+    print("In Test")
+    print()
+    return render_template('hello.html')
+
+if __name__ == "__main__":
+    app.run\
+        (
+            host="0.0.0.0",
+            port=5000,
+            debug=True
+#            host=app.config["FLASK_HOST"],
+#            port=app.config["FLASK_PORT"],
+#            debug=app.config["FLASK_DEBUG"]
+        )
