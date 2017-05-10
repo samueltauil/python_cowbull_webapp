7a5606490e9ebe68b0aa5b4c0285f25aa706092b
David Sanders <dsanderscanada@gmail.com>
2017-05-10 13:50:16 -0400

    Updating index.html

--- GameSPA/GameSPA.py
+++ GameSPA/GameSPA.py
@@ -1,8 +1,14 @@
 import json
-from flask import render_template
+from flask import render_template, request
 from flask.views import MethodView
+from python_cowbull_game.GameObject import GameObject
 
 
 class GameSPA(MethodView):
     def get(self):
-        return render_template("index.html")
+        digits = GameObject.digits_used
+        guesses = GameObject.guesses_allowed
+        game_modes = str([mode for mode in GameObject.digits_used])\
+            .replace('[','').replace(']','').replace("'",'')
+
+        return render_template("index.html", digits, guesses, game_modes)
