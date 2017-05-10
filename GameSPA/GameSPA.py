6babac91723c23c1265275df8f6925bad5dbc117
David Sanders <dsanderscanada@gmail.com>
2017-05-10 14:04:25 -0400

    Updating index.html

--- GameSPA/GameSPA.py
+++ GameSPA/GameSPA.py
@@ -10,5 +10,18 @@ class GameSPA(MethodView):
         guesses = GameObject.guesses_allowed
         game_modes = str([mode for mode in GameObject.digits_used])\
             .replace('[','').replace(']','').replace("'",'')
+        table = []
+        for mode in game_modes:
+            table.append({
+                "mode": mode,
+                "digits": digits[mode],
+                "guesses": guesses[mode]
+            })
 
-        return render_template("index.html", digits=digits, guesses=guesses, game_modes=game_modes)
+        return render_template(
+            "index.html",
+            digits=digits,
+            guesses=guesses,
+            game_modes=game_modes,
+            modes_table=table
+        )
