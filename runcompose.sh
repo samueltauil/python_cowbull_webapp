8c53b345d0724977ad2586e6b47b4ac800f1de84
David J. Sanders <dsanderscanada@gmail.com>
2018-09-18 22:21:14 -0400

    Update docker-compose options

---
+++ runcompose.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export BUILD_NUMBER="COMPOSE"; \
+  export COWBULL_ENVIRONMENT="Local Compose";\
+  export COWBULL_VERSION="v1"; \
+  export app_srv="cowbull"; \
+  export app_srv_version="1.0.172"; \
+  export doclog=dsanderscan; \
+  docker-compose \
+    -f vendor/docker/docker-compose.yml \
+    up -d --build
